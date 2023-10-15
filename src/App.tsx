35d6f47bd5844b0e7c1e125a6433a4f0b810a4fe
HeinZawPhyoe <hheinzzawpphyoe@gmail.com>
2023-10-15 22:49:19 +0630

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ const App = () => {
   return (
     <div>
       <div className="container flex justify-evenly mx-auto py-3 text-white cursor-pointer bg-blue-400 ">
-        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16" >Home</h2>
-        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16" >Features</h2>
-        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16" >Company</h2>
-        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16" >Pricing</h2>
-        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16" >Customers</h2>
-        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16" >Support</h2>
+        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16">
+          Home
+        </h2>
+        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16">
+          Features
+        </h2>
+        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16">
+          Company
+        </h2>
+        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16">
+          Pricing
+        </h2>
+        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16">
+          Customers
+        </h2>
+        <h2 className=" hover:underline-offset-2 hover:underline hover:text-red-500 active:rotate-[360deg] duration-500 hover:scale-150 transform  active:translate-y-16">
+          Support
+        </h2>
       </div>
+      <div>Test Git</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
